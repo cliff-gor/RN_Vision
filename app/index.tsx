c435a6db141925486182a2f245d0ef3425cc15b0
cliff-gor <cliff.gor.dev@gmail.com>
2025-02-05 16:21:30 +0300

    Add zoom controls state, zoom controls component

--- app/index.tsx
+++ app/index.tsx
@@ -9,14 +9,17 @@ import {
   StatusBar,
   StyleSheet,
   Text,
+  TouchableHighlight,
   View,
 } from "react-native";
 import {
   Camera,
   useCameraDevice,
   useCameraPermission,
+  TakePhotoOptions,
 } from "react-native-vision-camera";
 import { BlurView } from "expo-blur";
+import { FontAwesome5 } from "@expo/vector-icons";
 
 export default function HomeScreen() {
   const { hasPermission } = useCameraPermission();
@@ -28,12 +31,33 @@ export default function HomeScreen() {
     "back"
   );
   const device = useCameraDevice(cameraPosition);
+  const [showZoomControls, setShowZoomControls] = React.useState(false);
+  const [showExposureControls, setShowExposureControls] = React.useState(false);
   const [zoom, setZoom] = React.useState(device?.neutralZoom);
   const [exposure, setExposure] = React.useState(0);
   const [flash, setFlash] = React.useState<"off" | "on">("off");
   const [torch, setTorch] = React.useState<"off" | "on">("off");
-
+  const camera = React.useRef<Camera>(null);
   const router = useRouter();
+
+  const takePicture = async () => {
+    try {
+      if (camera.current === null) throw new Error("Camera ref is null");
+      console.log("Taking Picture....");
+      const photo = await camera.current.takePhoto({
+        flash: flash,
+        enableShutterSound: false,
+      });
+
+      router.push({
+        pathname: "/media",
+        params: { media: photo.path, type: "photo" },
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
   if (redirectToPermissions) return <Redirect href={"/permissions"} />;
   if (!device) return <></>;
 
@@ -43,6 +67,7 @@ export default function HomeScreen() {
         {/* <ThemedText>Hello Camera</ThemedText> */}
         <View style={{ flex: 2 }}>
           <Camera
+            ref={camera}
             style={{ flex: 1, borderRadius: 10, overflow: "hidden" }}
             device={device}
             isActive
@@ -50,6 +75,8 @@ export default function HomeScreen() {
             resizeMode="cover"
             exposure={exposure}
             torch={torch}
+            video
+            photo
           />
 
           <BlurView
@@ -128,6 +155,33 @@ export default function HomeScreen() {
               iconName="settings-outline"
               onPress={() => router.push("/_sitemap")}
               containerStyle={{ alignSelf: "center" }}
+            />
+          </View>
+
+          {/* Bottom Section */}
+          <View
+            style={{
+              flex: 1.1,
+              flexDirection: "row",
+              justifyContent: "space-evenly",
+              alignItems: "center",
+            }}
+          >
+
+            <VisionButton 
+            iconSize={40}
+            title="+/-"
+            onPress={() => setShowZoomControls((s) => !s)}
+            containerStyle={{alignSelf: "center"}}
+            />
+            <TouchableHighlight onPress={takePicture}>
+              <FontAwesome5 name="dot-circle" size={55} color={"white"} />
+            </TouchableHighlight>
+            <VisionButton 
+            iconSize={40}
+            title="1x"
+            onPress={() => setShowExposureControls((s) => !s)}
+            containerStyle={{alignSelf: "center"}}
             />
           </View>
         </View>
